d3a1a7e6e4cee026275054ba066332c3c2515ebf
Jonathan R Lopez <lopezjr2334@student.laccd.edu>
2016-04-25 14:52:51 -0700

    server is good
    server up and running.
    you’ll need mime node_module.

--- server.js
+++ server.js
@@ -7,19 +7,61 @@ var http = require('http'),
 
 //I have to prototype it to make this work
 function resWriteHeadFn(num, contentObj) {
-	if(!num && !contentObj) return console.log('please enter number and ContentObj');
+	if(!num && !contentObj) return console.log('please enter number and '+
+		'ContentObj');
 	return writeHead(num, contentObj);
-}
-
+};
+//sending errors data goes here. 
 function send404(res) {
 	res.writeHead(404, {'Content-Type': 'text/plain'});
 	res.write('Error 404: resource not found.');
 	res.end();
-}
-
+};
+//sending data goes here. 
 function sendFile(res, filePath, fileContents) {
 	res.writeHead(200, 
 		{'Content-Type': mime.lookup(path.basename(filePath) ) } 
 	);
 	res.end(fileContents);
-}
+};
+
+//serving static files
+function serveStatic(res, cache, absPath) {
+	if(cache[absPath]) {
+		sendFile(res, absPath, cache[absPath]);
+	} else {
+		fs.exists(absPath, function(exists) {
+			if(exists) {
+				fs.readFile(absPath, function(err, data) {
+					if(err) {
+						send404(res);
+					}else {
+						cache[absPath] = data;
+						sendFile(res, absPath, data);
+					}
+				});
+			} else {
+				send404(res);
+			}
+		});
+	}
+};
+
+var server = http.createServer(
+	function(req, res) {
+		var filePath = false;
+		
+		if(req.url == '/') {
+			filePath = 'public/index.html';
+		} else {
+			filePath = 'public' + req.url;
+		}
+		
+		var absPath = './' + filePath;
+		
+		serveStatic(res, cache, absPath);
+	}
+);
+server.listen(3000, function() {
+	console.log('check 3000');
+}); 
