bbc0767a99eed3270ca3eef2234f8866fff20ee5
Jonathan <jonathanwork856@hotmail.com>
2016-04-27 12:35:09 -0700

    finished chat_server
    just finished chat server file.

---
+++ public/javascripts/chat.js
@@ -0,0 +1,43 @@
+var Chat = function(socket) {
+  this.socket = socket;
+}
+//send message
+Chat.prototype.sendMessage = function(room, text) {
+  var message = {
+    room: room,
+    text: text
+  };
+  this.socket.emit('message', message);
+};
+//change rooms
+Chat.prototype.changeRoom = function(room) {
+  this.socket.emit('join' , {
+    newRoom: room
+  });
+};
+Chat.prototype.processCommand = function(command) {
+  var words = command.split(' ');
+  var command = words[0].
+                        substring(1, words[0].length).
+                        toLowerCase();
+
+  var message = false;
+
+  switch (command) {
+    case 'join':
+      words.shift();
+      var room = words.join(' ');
+      this.changeRoom(room);
+      break;
+    default:
+    case: 'tom':
+      words.shift();
+      var name = words.join(' ');
+      this.socket.emit('nameAttempt', name);
+      break;
+    default:
+      message = 'Unrecoginzed command.';
+      break
+  }
+  return message;
+}
